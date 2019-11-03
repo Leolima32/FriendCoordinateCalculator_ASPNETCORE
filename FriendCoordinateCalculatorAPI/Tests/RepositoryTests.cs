d49dc4817935d11860e3ca86422fd55f1d634a47
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2019-11-03 08:23:28 -0300

    first test

---
+++ FriendCoordinateCalculatorAPI/Tests/RepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Infra.Interfaces;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class RepositoryTests
+    {
+        private Friend mockFriend;
+        private readonly IFriendsRepository _friendsRepository;
+        public RepositoryTests(IFriendsRepository reposioty)
+        {
+            mockFriend = new Friend("Alex", new Coordinate(100M, 200M));
+            _friendsRepository = reposioty;
+        }
+        [Fact]
+        public void ShouldAddFriend()
+        {
+            Assert.True(_friendsRepository.AddFriend(mockFriend));
+        }
+    }
+}
